6dd784293adfe74d7ad80eda4782f651ebcd08c2
ricardormeza <ricardo.rmeza@live.com>
2026-01-15 12:59:40 -0800

    fix error

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -104,7 +104,7 @@ export default function Page() {
     show: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.6, ease: [0.22, 1, 0.36, 1] },
+      transition: { duration: 0.6, ease: "easeOut" },
     },
   };
 
